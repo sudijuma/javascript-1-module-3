9e8a89d02845c508619aa944326204096e82f55d
Hesham <heshamelmasry1992@gmail.com>
2022-02-07 10:14:05 +0100

    lessons folder

---
+++ lesson-1/1-truly-falsy-example/js/script.js
@@ -0,0 +1,38 @@
+// *** List of falsy values ****
+//
+// false
+// 0 - the number zero
+// "", '' or - empty strings
+// null
+// undefined
+// Nan - Not a Number
+
+// Everything else is Truly
+
+
+// If we are trying to check if a value exists (if it isn't null or undefined) for example,
+// we can do this:
+
+//TODO write example
+
+
+// rather than having to check for both null and undefined like this:
+
+//TODO write example
+
+
+
+
+// That applies to all the falsy values,
+// so we can check that a variable's value is not one of the falsy values
+// by writing an if statement like this:
+
+
+
+
+
+// Example 1 Play with truly and falsy values
+
+
+
+// Example 2 Real life example
